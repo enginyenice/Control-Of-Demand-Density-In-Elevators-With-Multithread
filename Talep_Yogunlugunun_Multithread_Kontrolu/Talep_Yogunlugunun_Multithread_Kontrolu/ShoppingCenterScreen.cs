2f0dd487cd79a9b254f4f5dd1c92644eef97235f
Engin Yenice <43140289+nginY26@users.noreply.github.com>
2020-12-01 20:01:38 +0300

    Proje oluşturuldu

---
+++ Talep_Yogunlugunun_Multithread_Kontrolu/Talep_Yogunlugunun_Multithread_Kontrolu/ShoppingCenterScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Talep_Yogunlugunun_Multithread_Kontrolu
+{
+    public partial class ShoppingCenterScreen : Form
+    {
+        public ShoppingCenterScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
